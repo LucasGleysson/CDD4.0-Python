d55c5f67ec564312761e75c2712b9e5262b30e6b
lucasgleysson <dlucasgleysson@gmail.com>
2023-10-10 17:16:34 -0300

    numeros unicos

---
+++ EX11.py
@@ -0,0 +1,4 @@
+from tecateca import numero_unicos
+
+result = numero_unicos(1, 2, 2, 3, 4, 4, 5, 3, 6, 7, 6, 8)
+print(result)
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-a = '1'
-b = 'b'
-print(a.isnumeric())
-print(b.isnumeric())
+lista_numeros = [1,2, 2, 3, 4, 4, 5, 3, 6, 7, 6, 8]
+nova_lista = [0] * len(lista_numeros)
+lista_temp = [0]
+tamanho = 0
+for i in range(len(lista_numeros)):
+    if lista_numeros[i] == 0:
+        continue
+    if lista_numeros[i] not in nova_lista:
+        nova_lista[i] = lista_numeros[i]
+        tamanho = 0
+nova_nova_lista = [0] * tamanho
+print(nova_lista)
